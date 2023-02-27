d68c5f002703ba9cd762b24f7693c74128decf50
Yurtaev Igor <yurtay2017@yandex.ru>
2023-02-27 16:28:48 +0500

    add get user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
+const auth = require("../middleware/auth.middleware");
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).send(users);
